f3d37559deda2288938b9d346641032610ff75a7
wang_ds <122259337+chenkai-wds@users.noreply.github.com>
2024-07-16 21:49:03 +0800

    update deep.py

--- Week1/deep.py
+++ Week1/deep.py
@@ -1,4 +1,4 @@
-answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
+answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 answer = answer.strip().lower()
 if answer == "42" or answer == "forty two" or answer == "forty-two":
     print("Yes")
